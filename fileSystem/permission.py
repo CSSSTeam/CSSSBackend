c032a9272cd1df3f5ce296fbfaf424547c49ab5b
jaanonim <mat8mro@gmail.com>
2020-02-27 20:12:43 +0100

    remove debag lines

--- fileSystem/permission.py
+++ fileSystem/permission.py
@@ -6,11 +6,9 @@ from users.utility import getUser, userHasPerm
 class canShow(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return True
         return userHasPerm(request, "filesystem.show")
 
 class canCreate(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return True
         return userHasPerm(request, "filesystem.create")
