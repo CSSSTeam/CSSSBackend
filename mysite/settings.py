ce35bc2804063e3cc8153366b3fc6f01e99619e5
jaanonim <mat8mro@gmail.com>
2020-02-27 22:26:44 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'CSSS',
-        'USER': 'csssUser',
+        'USER': 'root',
         'PASSWORD': '1234',
         'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
