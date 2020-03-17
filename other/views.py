422270156a2c63e1c208fb6e390276ddd08a05bc
jaanonim <mat8mro@gmail.com>
2020-03-17 16:07:40 +0100

    permissions for other

--- other/views.py
+++ other/views.py
@@ -1,9 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from django.utils import timezone
+from other.permission  import canShow
 
 #------------------------GET-------------------------
 @api_view(['GET'])
+@permission_classes([canShow])
 def now(request):
     response = {"date": timezone.now()}
     return Response(response)
