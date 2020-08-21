736138c348627f1755585cdc8131b21c7a4cc34f
jaanonim <mat8mro@gmail.com>
2020-08-21 21:26:28 +0200

    default for events.type.color (set to black)

--- events/models.py
+++ events/models.py
@@ -6,7 +6,7 @@ import uuid
 class type (models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
-    color = models.CharField(max_length=6)
+    color = models.CharField(max_length=6,default='000000')
 
 
 class event (models.Model):
