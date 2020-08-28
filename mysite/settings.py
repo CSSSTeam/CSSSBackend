5d88ef363678b62e000a82fc70f98726e2a8432d
jaanonim <mat8mro@gmail.com>
2020-08-28 15:18:17 +0200

    settings small fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,9 +44,10 @@ CREATE_DEBUG_USERS = True
 # Don't remove this if
 if not DEBUG:
     CREATE_DEBUG_USERS = False
+    SECURE_SSL_REDIRECT = True
 
 ALLOWED_HOSTS = ['csssinf.herokuapp.com']
-SECURE_SSL_REDIRECT = True
+
 
 # Application definition
 INSTALLED_APPS = ['django.contrib.admin',
@@ -102,9 +103,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'CSSS',
         'USER': 'root',
-        'PASSWORD': 'password',
-        'HOST': '0.0.0.0',  # Or an IP Address that your DB is hosted on
-        'PORT': '3300',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
